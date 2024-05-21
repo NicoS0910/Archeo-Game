b647817843508e94e50445842cb29abae3640798
NicoS0910 <116800832+NicoS0910@users.noreply.github.com>
2024-05-21 12:26:25 +0200

    Nico (#5)
    * Added a Main Menu Scene and imported TextMeshPro
    
    * Implemented Main Menu
    
    * Added a Pause Menu
    
    * Placement of Resume Button
    
    * Pause Button Placement
    
    * Pause-Menu auf ESC und Options Menu dabei

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,25 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-   
-   public GameObject PausePanel;
+    public GameObject pausePanel; // Zuweisung des Pausenmenü-Panels im Inspektor
+    private bool isPaused = false; // Status, ob das Spiel pausiert ist
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Prüfen, ob die Escape-Taste gedrückt wird
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Escape-Taste gedrückt");
+            if (isPaused)
+            {
+                Debug.Log("Spiel wird fortgesetzt");
+                Continue(); // Fortsetzen des Spiels, wenn es pausiert ist
+            }
+            else
+            {
+                Debug.Log("Spiel wird pausiert");
+                Pause(); // Pausieren des Spiels, wenn es nicht pausiert ist
+            }
+        }
     }
 
-    public void Pause() {
-        PausePanel.SetActive(true);
-        Time.timeScale = 0;
+    public void Pause()
+    {
+        pausePanel.SetActive(true); // Aktivieren des Pausenmenü-Panels
+        Time.timeScale = 0f; // Anhalten der Spielzeit
+        isPaused = true; // Status auf pausiert setzen
+        Debug.Log("Pausenmenü aktiviert");
     }
 
-    public void Continue() {
-          PausePanel.SetActive(false);
-           Time.timeScale = 1;
+    public void Continue()
+    {
+        pausePanel.SetActive(false); // Deaktivieren des Pausenmenü-Panels
+        Time.timeScale = 1f; // Fortsetzen der Spielzeit
+        isPaused = false; // Status auf nicht pausiert setzen
+        Debug.Log("Pausenmenü deaktiviert");
     }
 }
