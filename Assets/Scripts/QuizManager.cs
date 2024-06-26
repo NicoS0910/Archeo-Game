da53dd3a887125c142a10c4e9e0b2cd8b7240833
Mecki <96236752+Nimixam@users.noreply.github.com>
2024-06-26 15:03:02 +0200

    QuizGame basic fertig

---
+++ Assets/Scripts/QuizManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuizManager : MonoBehaviour
+{
+    public Button correctAnswerButton;   // Der Button, der die korrekte Antwort darstellt
+    public Button[] wrongAnswerButtons;  // Die Buttons, die falsche Antworten darstellen
+
+    void Start()
+    {
+        // Fügen Sie jedem Button einen Listener hinzu, um auf Klicks zu reagieren
+        correctAnswerButton.onClick.AddListener(() => OnCorrectButtonClick(correctAnswerButton));
+        
+        foreach (Button button in wrongAnswerButtons)
+        {
+            button.onClick.AddListener(() => OnWrongButtonClick(button));
+        }
+    }
+
+    void OnCorrectButtonClick(Button clickedButton)
+    {
+        // Markiere den korrekten Button grün
+        SetButtonColor(clickedButton, Color.green);
+
+        // Deaktiviere alle falschen Buttons
+        DisableWrongButtons();
+    }
+
+    void OnWrongButtonClick(Button clickedButton)
+    {
+        // Markiere den falschen Button rot
+        SetButtonColor(clickedButton, Color.red);
+    }
+
+    void DisableWrongButtons()
+    {
+        foreach (Button button in wrongAnswerButtons)
+        {
+            button.interactable = false;
+        }
+    }
+
+    void SetButtonColor(Button button, Color color)
+    {
+        // Ändern Sie die Farbe des Button-Hintergrunds
+        ColorBlock colors = button.colors;
+        colors.normalColor = color;
+        button.colors = colors;
+    }
+}
