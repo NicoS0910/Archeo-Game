72e562740db996b4a31e5f07e599b990e8adfa10
Mecki <96236752+Nimixam@users.noreply.github.com>
2024-08-13 11:53:52 +0200

    i: Artifact Inventory
    - Sword im Artifact Inventory hinzugefügt
    - Frames + green screen eingefügt
    - bei zu langem Text im Artefakt-Inventar kann man jetzt mit linksklick weiterlesen / seite wechsen

---
+++ Assets/Scripts/ToggleObjectsOnClick.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ToggleUIObjectsOnRightClick : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject object1;
+    public GameObject object2;
+
+    private bool isObject1Active = true;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // Überprüfen, ob der Rechtsklick (MouseButton 1) verwendet wurde
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            ToggleObjects();
+        }
+    }
+
+    void ToggleObjects()
+    {
+        isObject1Active = !isObject1Active;
+
+        object1.SetActive(isObject1Active);
+        object2.SetActive(!isObject1Active);
+    }
+}
