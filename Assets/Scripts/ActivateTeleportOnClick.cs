28c1485beb3e956f0520e25a2a5f8f3e7ab7a06b
Mecki <96236752+Nimixam@users.noreply.github.com>
2024-08-28 15:57:38 +0200

    E-Scooter Scene WIP

--- Assets/Scripts/ActivateTeleportOnClick.cs
+++ Assets/Scripts/ActivateTeleportOnClick.cs
@@ -1,14 +1,32 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
-public class ActivateTeleportOnClick : MonoBehaviour
+public class ActivateTeleportOnClick : MonoBehaviour, IPointerClickHandler
 {
     public int sceneBuildIndex; // Der Build-Index der neuen Szene, die geladen werden soll
 
-    // Methode zum Laden der neuen Szene
-    public void OnButtonClicked()
+    // Methode, die beim Klicken auf den Button ausgeführt wird
+    public void OnPointerClick(PointerEventData eventData)
     {
-        // Lade die neue Szene basierend auf dem Build-Index
-        SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+        // Überprüfen, ob die linke Maustaste gedrückt wurde
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            LoadScene();
+        }
+    }
+
+    private void LoadScene()
+    {
+        // Überprüfen, ob der Build-Index gültig ist
+        if (sceneBuildIndex >= 0 && sceneBuildIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            // Lade die neue Szene basierend auf dem Build-Index
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+        }
+        else
+        {
+            Debug.LogError("Invalid sceneBuildIndex! Please check the Build Settings.");
+        }
     }
 }
